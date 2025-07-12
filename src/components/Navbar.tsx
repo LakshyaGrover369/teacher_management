b47c4d83ac91734d0efab9cc14c805cab24488a1
lakshya369 <groverlakshya.25.lg@gmail.com>
2025-07-12 17:03:01 +0530

    changed the README and name of the project

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
         <div className="flex justify-between h-16">
           <div className="flex">
             <div className="flex-shrink-0 flex items-center">
-              <h1 className="text-xl font-bold text-indigo-600">TeachManage</h1>
+              <h1 className="text-xl font-bold text-indigo-600">EduConnect</h1>
             </div>
           </div>
           <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
